d05e055bede7b011b3f2f17e8c94099477ad002f
sumonDev9 <sumonmit678@gmail.com>
2025-01-07 00:09:24 +0530

    Add AdminRoute component for protecting admin-only routes

--- src/providers/PrivateRoute.jsx
+++ src/providers/PrivateRoute.jsx
@@ -1,10 +1,9 @@
-import React, { useContext } from 'react';
-import { AuthContext } from './Authprovider';
-import Loading from '../components/Loading';
+
 import { Navigate, useLocation } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
 
 const PrivateRoute = ({children}) => {
-    const {user, loading} = useContext(AuthContext);
+    const {user, loading} = useAuth()
     const location = useLocation();
     
     if(loading){
