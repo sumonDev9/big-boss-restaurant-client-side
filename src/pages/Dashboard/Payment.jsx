9ac73fd351d08471a88ebadef2de75023d2cfa08
sumonDev9 <sumonmit678@gmail.com>
2025-01-10 10:24:39 +0530

    Set up the Payment component with Stripe integration and placeholder Elements wrapper

--- src/pages/Dashboard/Payment.jsx
+++ src/pages/Dashboard/Payment.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import SectionHeading from '../../components/sectionHeading';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 
+
+// TODO: add publishable key
+const stripePromise = loadStripe('')
 const Payment = () => {
     return (
         <div>
             <SectionHeading heading="Payment" subHeading='Please pay to eat'></SectionHeading>
+            <div>
+                <Elements stripe={stripePromise}>
+
+                </Elements>
+            </div>
         </div>
     );
 };
